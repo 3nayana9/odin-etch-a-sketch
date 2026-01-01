142706e72114d15ec13c5e503dfa229483e7f1d4
3nayana9 <pattoorithrinayana@gmail.com>
2026-01-01 21:42:34 +0530

    Created the hovering effect with colours

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ for(let i = 0;i<n;i++){
         grid.appendChild(cell);
     }
 }
+
+
+
+grid.addEventListener('mouseover', (e) => {
+    if (e.target.classList.contains('box')) {
+
+        // generate random RGB values
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+
+        // set the hovered box's background
+        e.target.style.backgroundColor = `rgb(${r},${g},${b})`;
+
+        console.log('entered', e.target);
+    }
+});
